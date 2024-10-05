6a8fd6c7e96d064374992b5dd30b7cddf784d467
katiedelson <katherine.edelson@gmail.com>
2024-10-05 18:18:01 -0400

    update

--- devlog.md
+++ devlog.md
@@ -5,9 +5,16 @@
 * [Other docs](todo)
 
 
-### 2024.09.24 - 2hr: Set up repo, itch.io, trello, and this!
+### 2024.09.24 - 2hrs: Set up repo, itch.io, trello, and this!
 * need to make new ssh key without passphrase :(
 
+### 2024.10.05 - 4hrs: Set up test tilemap & started on wall jumping & sliding
+* put together basic level with custom tileset I already had made prior
+* found tutorial: and https://www.youtube.com/watch?v=2PiMrxGP2_o which will help me with the wall jump mechanic
+* ran into some problems with the is_on_wall() func as I wanted it to be true if it was
+  near wall rather than actively running into, found more indepth tutorial https://www.youtube.com/watch?v=CStyW2aTBSo
+  which helped create separate function is_near_wall with a RayCast2D node
+* having issues with smoothness & code organization, might overhaul the given script for player physics
 examples============================
 
 ### 2024-09-23 - 6.5hr: Headline of what work was done like "Got the player character working."
--- scenes/player.gd
+++ scenes/player.gd
@@ -3,31 +3,47 @@ extends CharacterBody2D
 
 const SPEED = 180.0
 const JUMP_VELOCITY = -300.0
+const WALL_SLIDING_SPEED = 3000
+
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var wall_checker: RayCast2D = $WallChecker
 
+
+var jumpsMade = 0
+var doWallJump = false
 
 func _physics_process(delta: float) -> void:
+	var direction = Input.get_axis("move_left", "move_right")
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+	if is_near_wall():
+		velocity.y = WALL_SLIDING_SPEED * delta
+	elif not is_on_floor(): velocity += get_gravity() * delta
+	else: jumpsMade = 0
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_near_wall():
+			velocity.y = JUMP_VELOCITY
+			velocity.x = -direction + SPEED
+			doWallJump = true
+			
+			$"../WallJumpTimer".start()
+		elif is_on_floor() || jumpsMade <2:
+			velocity.y = JUMP_VELOCITY
+			jumpsMade += 1
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || jumpsMade <2):
 		velocity.y = JUMP_VELOCITY
-
+		jumpsMade += 1
 	# Get the input direction and handle the movement/deceleration.
 	
 
 		
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
 	
 	#flip direction
-	if direction >0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+
 	
 	#play animations
 	if is_on_floor():
@@ -40,9 +56,25 @@ func _physics_process(delta: float) -> void:
 	
 	
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
+	if direction && not doWallJump: velocity.x = direction * SPEED
+	elif not doWallJump:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	set_direction()
 	move_and_slide()
+	
+func set_direction():
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		animated_sprite.flip_h = false
+		wall_checker.rotation_degrees = -90
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		wall_checker.rotation_degrees = 90
+	#wall_checker.rotation_degrees = 90 * -direction
+		
+func is_near_wall():
+	return $WallChecker.is_colliding()
+
+func _on_wall_jump_timer_timeout() -> void:
+	doWallJump = false
